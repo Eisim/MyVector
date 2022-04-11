10ca47c4da523ef894ab84c7384d5d06f68df1f7
Eisim <92314213+Eisim@users.noreply.github.com>
2022-04-11 20:58:22 +0300

    Add files via upload

---
+++ MyVector(v1)/MyVector(v1)/main.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include"MyVector.h"
+//vector (n lenght)
+//type:double 
+//+ - *(const) 
+//normali
+
+int main() {
+	Vector a(3);
+	Vector b(3);
+	Vector c(3);
+	a.randomfill();
+	b.randomfill();
+	std::cout << "a=";
+	a.print();
+	std::cout << "b=";
+	b.print();
+
+
+
+
+	std::cout << "c=";
+	c.print();
+	std::cout << "---c=a+b---";
+	c = a + b;
+	c.print();;
+	std::cout << "---c=a-b---";
+	c = a - b;
+	c.print();
+	std::cout << "---c=a*2---";
+	c = a * 2;
+	c.print();
+	std::cout << "---c=2*a---";
+	c = 2 * a;
+	c.print();
+	std::cout << "---norm c:" << c.getNorm() << std::endl;
+	std::cout << "---a+=b---";
+	a += b;
+	a.print();
+	std::cout << "---b-=a---";
+	b -= a;
+	b.print();
+	std::cout << "---b*=-1---";
+	b *= -1;
+	b.print();
+	std::cout << "a=" << a;
+	std::cout << "b=" << b;
+	std::cout << "c=" << c;
+
+	std::cout << "size a:" << a.getSize() << std::endl;
+	std::cout << "dot(a,b):" << a.dot(b) << std::endl;
+	std::cout << "cout:" << a << std::endl;
+
+	return 0;
+}
